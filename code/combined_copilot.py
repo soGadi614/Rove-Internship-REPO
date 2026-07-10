6a024e858e93e901039c411bd1bb04d1d22a8dda
soGadi614 <sogadi14@gmail.com>
2026-07-09 19:51:02 -0500

    Update combined_copilot.py

--- code/combined_copilot.py
+++ code/combined_copilot.py
@@ -3,10 +3,9 @@ import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 # Your existing imports continue below...
-from rag_loop import suggest_answer
-from sop_match import match_sop
-from Refund_Calculator import Refund_Calculator
-
+from code.rag_loop import suggest_answer
+from code.sop_match import match_sop
+from code.Refund_Calculator import Refund_Calculator
 
 REFUND_KEYWORDS = [
     "refund", "cancel", "canceled", "cancelled", "cancellation",
